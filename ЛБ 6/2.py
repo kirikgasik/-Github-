4deae74fe8cf8d735103a6bb5224a00c9ecead81
kirikgasik <gasilov0305@gmail.com>
2025-12-25 20:40:49 +0500

    Delete ЛБ 6 directory

--- ЛБ 6/2.py
+++
@@ -1,8 +0,0 @@
-text = input("Введите текст: ")
-start, end = map(int, input("Введите диапазон (например, 3 7):").split())
-
-start_index = start - 1
-end_index = end
-
-substring = text[start_index:end_index]
-print(f"Результат: {substring}")
