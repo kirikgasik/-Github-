4871605cd0ed25aeb71349838298f5e2f88ec6c9
kirikgasik <gasilov0305@gmail.com>
2025-12-26 22:24:46 +0500

    Delete deadline_3LR3 directory

--- deadline_3LR3/3.py
+++
@@ -1,9 +0,0 @@
-def find_max(arr):
-    if len(arr) == 1:
-        return arr[0]
-    mid = len(arr) // 2
-    left_max = find_max(arr[:mid])
-    right_max = find_max(arr[:mid])
-    return left_max if left_max > right_max else right_max
-numbers = [6, 4, 2, 5, 9, 12]
-print(find_max(numbers))
